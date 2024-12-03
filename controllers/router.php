a8a5ccbf9f026f784e6b4ff8dce94dfcd3673ab2
Varga Márk János 2 <vrgmrkjanos@gmail.com>
2024-12-03 11:51:34 +0100

    előkészülés

---
+++ controllers/router.php
@@ -0,0 +1,74 @@
+<?php
+
+session_start();
+if(! isset($_SESSION['userid'])) $_SESSION['userid'] = 0;
+if(! isset($_SESSION['userfirstname'])) $_SESSION['userfirstname'] = "";
+if(! isset($_SESSION['userlastname'])) $_SESSION['userlastname'] = "";
+if(! isset($_SESSION['userlevel'])) $_SESSION['userlevel'] = "1__";
+
+include(SERVER_ROOT . 'includes/database.inc.php');
+include(SERVER_ROOT . 'includes/menu.inc.php');
+
+// Felbontjuk a param�tereket. Az & elv�laszt� jellel v�gzett felbont�s
+// megfelel� lesz, els� eleme a megtekinteni k�v�nt oldal neve.
+
+$page = "nyitolap";
+$subpage = "";
+$vars = array();
+
+$request = $_SERVER['QUERY_STRING'];
+
+if($request != "")
+{
+	$params = explode('/', $request);
+	$page = array_shift($params); // a k�rt oldal neve
+	
+	if(array_key_exists($page, Menu::$menu) && count($params)>0) // Az oldal egy men�pont oldala �s van m�g adat az url-ben
+	{
+		$subpage = array_shift($params); // a k�rt aloldal
+		if(! (array_key_exists($subpage, Menu::$menu) && Menu::$menu[$subpage][1] == $page)) // ha nem egy alolal
+		{
+			$vars[] = $subpage; // akkor ez egy parameter
+			$subpage = ""; // �s nincs aloldal
+		}
+	}
+	$vars += $_POST;
+	
+	foreach($params as $p) // a param�terek t�mbje felt�lt�se
+	{
+		$vars[] = $p;
+	}
+}
+
+// Meghat�rozzuk a k�rt oldalhoz tartoz� vez�rl�t. Ha megtal�ltuk
+// a f�jlt �s a hozz� tartoz� vez�rl� oldalt is, akkor bet�ltj�k az
+// el�bbiekben lek�rdezett param�tereket tov�bbadva. 
+
+$controllerfile = $page.($subpage != "" ? "_".$subpage : "");
+$target = SERVER_ROOT.'controllers/'.$controllerfile.'.php';
+if(! file_exists($target))
+{
+	$controllerfile = "error404";
+	$target = SERVER_ROOT.'controllers/error404.php';
+}
+
+include_once($target);
+$class = ucfirst($controllerfile).'_Controller';
+if(class_exists($class))
+	{ $controller = new $class; }
+else
+	{ die('class does not exists!'); }
+
+// spl_autoload_register(...) f�ggv�ny, amely ismeretlen oszt�ly h�v�sakor, megpr�b�lja automatikusan bet�lteni a megfelel� f�jlt. 
+// A modellekhez haszn�ljuk, egys�gesen nevezz�k el f�jljainkat (oszt�ly nev�vel megegyez�, csupa kisbet�s .php)
+spl_autoload_register(function($className) {
+    $file = SERVER_ROOT.'models/'.strtolower($className).'.php';
+    if(file_exists($file))
+    { include_once($file); }
+    else
+    { die("File '$filename' containing class '$className' not found.");    }
+});
+
+$controller->main($vars);
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/web2/');
+
+define('SITE_ROOT', 'http://localhost/web2/');
+
+require_once(SERVER_ROOT . 'controllers/' . 'router.php');
+?>
