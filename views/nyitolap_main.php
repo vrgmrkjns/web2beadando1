1e7948bfbf0c237a75f2377bfa289f337c5a2889
Varga Márk János 2 <vrgmrkjanos@gmail.com>
2024-12-03 14:01:49 +0100

    404 nem működik valamiért, otthon folyt

---
+++ views/nyitolap_main.php
@@ -0,0 +1,122 @@
+<div class="hero">
+      <div class="hero-slide">
+        <div
+          class="img overlay"
+          style="background-image: url('images/hero_bg_3.jpg')"
+        ></div>
+        <div
+          class="img overlay"
+          style="background-image: url('images/hero_bg_2.jpg')"
+        ></div>
+        <div
+          class="img overlay"
+          style="background-image: url('images/hero_bg_1.jpg')"
+        ></div>
+      </div>
+
+      <div class="container">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-lg-9 text-center">
+            <h1 class="heading" data-aos="fade-up">
+              Balatoni hajók
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <div class="row mb-5 align-items-center">
+                <div class="col-lg-6">
+                    <h2 class="font-weight-bold text-primary heading">
+                    Rólunk
+                    </h2>
+                    <p class="font-weight-bold text-primary heading">
+                    A Balaton Hajó Nyilvántartó Kft. azért alakult, hogy hatékonyan nyomon kövesse és rendszerezze a tó területén közlekedő hajók adatait. Céljuk, hogy biztosítsák a hajóflotta biztonságos és átlátható működését, valamint elősegítsék a hajózási rend betartását.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section">
+      <div class="container">
+        <div class="row mb-5 align-items-center">
+          <div class="col-lg-6">
+            <h2 class="font-weight-bold text-primary heading">
+              Népszerű hajók
+            </h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="property-slider-wrap">
+              <div class="property-slider">
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/szent_miklos.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >Siófoki kikötő</span
+                      >
+                      <span class="city d-block mb-3">Szent Miklós</span>
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >Részletek</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/helka.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >Balatonfüredi kikötő</span
+                      >
+                      <span class="city d-block mb-3">Helka</span>
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >Részletek</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+
+                <div class="property-item">
+                  <a href="property-single.html" class="img">
+                    <img src="images/kisfaludy.jpg" alt="Image" class="img-fluid" />
+                  </a>
+
+                  <div class="property-content">
+                    <div>
+                      <span class="d-block mb-2 text-black-50"
+                        >Balatonfüredi kikötő</span
+                      >
+                      <span class="city d-block mb-3">Kisfaludy</span>
+                      <a
+                        href="property-single.html"
+                        class="btn btn-primary py-2 px-3"
+                        >Részletek</a
+                      >
+                    </div>
+                  </div>
+                </div>
+                <!-- .item -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
