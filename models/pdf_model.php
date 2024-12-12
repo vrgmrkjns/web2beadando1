3de2f028ecdf149594ca3f054855962c2e6f9518
Varga Márk János <varag.mark@gmail.com>
2024-12-12 21:16:27 +0100

    sok változás

--- models/pdf_model.php
+++ models/pdf_model.php
@@ -2,10 +2,63 @@
 require_once '../includes/database.inc.php';
 
 $dbh = Database::getConnection();
+// Form adatok megszerzése
+$nev = isset($_POST['nev']) ? $_POST['nev'] : '';
+$tipus = isset($_POST['tipus']) ? $_POST['tipus'] : '';
+$uzemel = isset($_POST['uzemel']) ? $_POST['uzemel'] : '';
 
-$sql = "SELECT * FROM hajo";
-$sth = $dbh->query($sql);
-$rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+// SQL lekérdezés kezdete
+$sql = "SELECT hajo.az, hajo.nev AS hajnev, hajo.tipus, tulajdonos.nev AS tulnev, 
+        CASE WHEN hajo.uzemel = 1 THEN 'Igen' 
+             WHEN hajo.uzemel = 0 THEN 'Nem' 
+             ELSE 'Ismeretlen' END AS uzemel,
+        GROUP_CONCAT(tort.nev ORDER BY tort.nev SEPARATOR ', ') AS tortnev
+        FROM hajo
+        INNER JOIN tulajdonos ON tulajdonos.az = hajo.tulaz
+        LEFT JOIN tort ON hajo.az = tort.hajoaz";
+
+// Dinamikus szűrés hozzáadása az SQL-hez
+$conditions = [];
+if (!empty($nev)) {
+    $conditions[] = "hajo.nev LIKE :nev";
+}
+if (!empty($tipus)) {
+    $conditions[] = "hajo.tipus = :tipus";
+}
+if ($uzemel !== '') {
+    $conditions[] = "hajo.uzemel = :uzemel";
+}
+
+// Ha van szűrő, hozzáadjuk őket a WHERE részhez
+if (count($conditions) > 0) {
+    $sql .= " WHERE " . implode(" AND ", $conditions);
+}
+
+// Csoportosítás
+$sql .= " GROUP BY hajo.az, hajo.nev, hajo.tipus, tulajdonos.nev, tulajdonos.varos";
+
+// Adatbázis kapcsolat és lekérdezés
+try {
+    $pdo = Database::getConnection();
+    $sth = $pdo->prepare($sql);
+    
+    // Paraméterek kötése
+    if (!empty($nev)) {
+        $sth->bindValue(':nev', "%" . $nev . "%");
+    }
+    if (!empty($tipus)) {
+        $sth->bindValue(':tipus', $tipus);
+    }
+    if ($uzemel !== '') {
+        $sth->bindValue(':uzemel', $uzemel);
+    }
+    
+    // Lekérdezés futtatása
+    $sth->execute();
+    $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Hiba történt: " . $e->getMessage();
+}
 
 require_once('../tcpdf/tcpdf.php');
 
@@ -14,68 +67,60 @@ false);
 
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Web-programozás II');
-$pdf->SetTitle('FELHASZNÁLÓK');
-$pdf->SetSubject('Web-programozás II - 3. Labor - TCPDF');
-$pdf->SetKeywords('TCPDF, PDF, Web-programozás II, Labor3');
+$pdf->SetTitle('Hajók');
+$pdf->SetSubject('Web-programozás II - Beadandó - TCPDF');
+$pdf->SetKeywords('TCPDF, PDF, Web-programozás II, Beadandó');
+
+$pdf->AddPage();
+
 
 $html = '
 <html>
 <head>
 <style>
-table {border-collapse: collapse;}
-th {font-weight: border: 1px solid red; text-align: center;}
-td {border: 1px solid blue;}
+table {border-collapse: collapse; width: 100%;}
+th {font-weight: bold; border: 1px solid red; text-align: center; padding: 8px;}
+td {border: 1px solid blue; text-align: center; padding: 8px;}
 </style>
 </head>
 <body>
-<h1 style="text-align: center; color: blue;">FELHASZNÁLÓK</h1>
+<h1 style="text-align: center; color: blue;">Hajók</h1>
 <table>
 <tr style="background-color: red; color: white;">
-<th style="width: 5%;">&nbsp;<br>&nbsp;<br>&nbsp;</th>
-<th style="width: 20%;">&nbsp;<br>CSALÁDI NÉV</th>
-<th style="width: 20%;">&nbsp;<br>UTÓNÉV</th>
-<th style="width: 20%;">&nbsp;<br>BEJELENTKEZÉS</th>
-<th style="width: 35%;">&nbsp;<br>JELSZÓ</th>
+<th style="width: 5%;">&nbsp;</th>
+<th style="width: 20%;">NÉV</th>
+<th style="width: 20%;">TÍPUS</th>
+<th style="width: 20%;">TULAJ</th>
+<th style="width: 15%;">ÜZEMEL?</th>
+<th style="width: 15%;">RÉGI NÉV</th>
 </tr>
 ';
-$i=1;
-foreach($rows as $row) {
-if($i)
-$html .= '
-<tr style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 255);">
-';
-else
-$html .= '
-<tr style="background-color: rgb(0, 0, 255); color: rgb(255, 255, 255);">
-';
-$j=0;
-foreach($row as $cell) {
-if($j==0)
-$html .= '
-<td style="text-align: right; width: 5%;">
-';
-else if($j < 4)
-$html .= '
-<td style="text-align: left; width: 20%;">
-';
-else if($j == 4)
-$html .= '
-<td style="text-align: left; width: 35%;">
-';
-$html .= $cell;
-$html .= '
-</td>
-';
-$j++;
+
+$i = 1;
+foreach ($rows as $row) {
+    $html .= $i ? '<tr style="background-color: rgb(255, 255, 255); color: rgb(0, 0, 255);">' : 
+                  '<tr style="background-color: rgb(0, 0, 255); color: rgb(255, 255, 255);">';
+
+    $j = 0;
+    foreach ($row as $cell) {
+        if ($j == 0) {
+            $html .= '<td style="text-align: right; width: 5%;">';
+        } else if ($j < 4) {
+            $html .= '<td style="text-align: left; width: 20%;">';
+        } else {
+            $html .= '<td style="text-align: left; width: 15%;">';
+        }
+
+        $html .= $cell;
+        $html .= '</td>';
+        $j++;
+    }
+    $html .= '</tr>';
+    $i = !$i;
 }
-$html .= '
-</tr>
-';
-$i = !$i;
-}
-$html .= '
-</table>
-<body>
+
+$html .= '</table>
+</body>
 </html>';
 
 $pdf->writeHTML($html, true, false, true, false, '');
